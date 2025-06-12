bc5286bc753a5f146e8166494bf9f7af99b8d471
teaguerchubak <teaguechubak@gmail.com>
2025-06-11 22:52:42 -0600

    reverting wpa changes

--- savestate.js
+++ savestate.js
@@ -39,26 +39,3 @@ window.addEventListener("load", () => {
 
 
 
-let deferredPrompt;
-const installBtn = document.getElementById("installBtn");
-
-window.addEventListener("beforeinstallprompt", (e) => {
-  e.preventDefault();
-  deferredPrompt = e;
-  installBtn.hidden = false;
-});
-
-installBtn.addEventListener("click", () => {
-  installBtn.hidden = true;
-  deferredPrompt.prompt();
-});
-
-
-
-
-
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("/service-worker.js")
-  .then(() => console.log("Service Worker registered!"))
-  .catch((err) => console.error("Service Worker failed:", err));
-}
